0a3f9860618d7c026f5c2faeb358e35ba9ceaa95
Choising <chltmdals115@gmail.com>
2021-06-30 23:58:10 +0900

    ex get controller

---
+++ src/main/kotlin/com/example/exkotlin/controller/get/GetApiController.kt
@@ -0,0 +1,40 @@
+package com.example.exkotlin.controller.get
+
+import com.example.exkotlin.controller.get.model.http.UserRequest
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("/api")
+class GetApiController {
+
+    @GetMapping(path = ["/hello", "/abcd"])
+    fun hello(): String {
+        return "hello kotlin"
+    }
+
+    @RequestMapping(method = [RequestMethod.GET], path = ["/request-mapping"])
+    fun requestMapping(): String {
+        return "request-mapping"
+    }
+
+    @GetMapping("/get-mapping/path-variable/{name}/{age}")
+    fun pathVariable(@PathVariable(value = "name") name: String, @PathVariable(name = "age") age: Int): String {
+        println("$name, $age")
+        return "$name $age"
+    }
+
+    @GetMapping("/get-mapping/query-param")
+    fun queryParam(
+        @RequestParam name: String,
+        @RequestParam(value = "age") age: Int
+    ): String {
+        println("$name, $age")
+        return "$name $age"
+    }
+
+    @GetMapping("/get-mapping/query-param/object")
+    fun queryParamObject(userRequest: UserRequest): UserRequest {
+        println(userRequest)
+        return userRequest
+    }
+}
---
+++ src/main/kotlin/com/example/exkotlin/controller/get/model/http/UserRequest.kt
@@ -0,0 +1,8 @@
+package com.example.exkotlin.controller.get.model.http
+
+data class UserRequest(var name:String?=null,
+                       var age:Int?=null,
+                       var email:String?=null,
+                       var address:String?=null) {
+
+}
