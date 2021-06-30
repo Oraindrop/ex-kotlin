80cd4b92c2cef97ad561bf8a2aff01cfe0e59c16
Choising <chltmdals115@gmail.com>
2021-06-30 23:34:05 +0900

    first commit

---
+++ src/test/kotlin/com/example/exkotlin/ExKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.exkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
