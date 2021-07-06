7878ea32a05c749689f353c8e8f168b918e68128
Choising <chltmdals115@gmail.com>
2021-07-06 23:44:18 +0900

    ex custom validation annotation

--- src/main/kotlin/com/example/exkotlin/model/http/UserRequest.kt
+++ src/main/kotlin/com/example/exkotlin/model/http/UserRequest.kt
@@ -1,5 +1,6 @@
 package com.example.exkotlin.model.http
 
+import com.example.exkotlin.annotation.StringFormatDateTime
 import java.lang.Exception
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -22,17 +23,6 @@ data class UserRequest(
     @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$")
     var phoneNumber:String?=null,
 
+    @field:StringFormatDateTime
     var createdAt:String?=null
-) {
-
-    @AssertTrue(message = "생성일자의 패턴은 yyyy-MM-dd HH:mm:ss 여야 합니다.")
-    private fun isValidCreatedAt(): Boolean {
-        return try {
-            LocalDateTime.parse(this.createdAt, DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
-            true
-        } catch (e : Exception) {
-            false
-        }
-    }
-
-}
+)
